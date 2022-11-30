cd29bb8276f4962278587504e608767f3dfb5967
Khan <gjf.kee@gmail.com>
2022-11-30 21:49:04 +0900

    Initial commit

---
+++ src/main/java/com/example/famlink/FamlinkApplication.java
@@ -0,0 +1,13 @@
+package com.example.famlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamlinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FamlinkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/famlink/FamlinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.famlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamlinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
